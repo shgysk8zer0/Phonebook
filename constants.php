88572e945b4dd008b2a50b6d9de7c61c4f2325d3
Chris Zuber <shgysk8zer0@gmail.com>
2016-08-02 14:31:40 -0700

    Update constants
    - Use correct manifest file
    - Add min PHP version, package version string, etc.

--- constants.php
+++ constants.php
@@ -1,6 +1,11 @@
 <?php
 namespace Constants;
 
-const MANIFEST      = './package.json';
-const TIMEZONE      = 'America/Los_Angeles';
-const ICON_SIZE     = 64;
+const MANIFEST        = './manifest.json';
+const TIMEZONE        = 'America/Los_Angeles';
+const ICON_SIZE       = 64;
+const CREDS           = './config/connect.json';
+const DEBUG           = true;
+const VERSION         = '0.0.0';
+const MIN_PHP_VERSION = '5.6';
+
