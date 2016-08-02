e9c92c92dbd477ace977ae0e92207785d4ab5d0e
Chris Zuber <shgysk8zer0@gmail.com>
2016-08-02 14:35:49 -0700

    Better error checking
    - Check that database credentials are available in an assert
    - Do not generate DOM/HTML if running as CLI

--- index.php
+++ index.php
@@ -1,19 +1,34 @@
 <?php
 namespace Index;
 use \Lib\Functions as Funcs;
+use \shgysk8zer0\Core as Core;
+use \shgysk8zer0\DOM as DOM;
+
+const VERSION = '0.0.0';
 
 require_once './constants.php';
-
-if (in_array(PHP_SAPI, ['cli', 'cli-server'])) {
-	require_once './lib/functions.php';
-}
+require_once './lib/functions.php';
 
 Funcs\init_assert();
-Funcs\php_version_check('5.6');
-Funcs\autoloader(['classes', '..', './config']);
-
+Funcs\php_version_check(\Constants\MIN_PHP_VERSION);
+Funcs\autoloader(['./classes']);
 date_default_timezone_set(\Constants\TIMEZONE);
 
-\shgysk8zer0\Core\Console::getInstance()->asErrorHandler()->asExceptionHandler();
-\shgysk8zer0\DOM\HTML::$echoOnExit = true;
-Funcs\load('head', 'body');
+assert('file_exists(\Constants\CREDS)', "Database credentials not found.");
+
+if (! Funcs\is_cli()) {
+	Core\Console::getInstance()->asErrorHandler()->asExceptionHandler();
+	DOM\HTML::$echoOnExit = true;
+	Funcs\load('head', 'body');
+	$pdo = Core\PDO::load(\Constants\CREDS);
+		$all = $pdo(
+		'SELECT
+		`displayName` as `name`,
+		`associatedNumbers` as `tel`,
+		`allLines` as `full text`
+		FROM `contacts`;'
+	);
+	$hash = md5(json_encode($all));
+	Core\Console::getInstance()->log($hash);
+}
+
