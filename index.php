49f21a1351a3882a8bf4b031e8f197241d639810
Chris Zuber <shgysk8zer0@gmail.com>
2016-08-05 12:56:02 -0700

    Clean-up and remove debuggin code

--- index.php
+++ index.php
@@ -4,31 +4,20 @@ use \Lib\Functions as Funcs;
 use \shgysk8zer0\Core as Core;
 use \shgysk8zer0\DOM as DOM;
 
-const VERSION = '0.0.0';
-
 require_once './constants.php';
 require_once './lib/functions.php';
 
-Funcs\init_assert();
 Funcs\php_version_check(\Constants\MIN_PHP_VERSION);
-Funcs\autoloader(['./classes']);
 date_default_timezone_set(\Constants\TIMEZONE);
-
-assert('file_exists(\Constants\CREDS)', "Database credentials not found.");
+Funcs\autoloader(['./classes']);
+Funcs\init_assert();
+assert(
+	'file_exists(\Constants\CREDS)',
+	sprintf("Database credentials not found. Create '%s'.", \Constants\CREDS)
+);
 
 if (! Funcs\is_cli()) {
 	Core\Console::getInstance()->asErrorHandler()->asExceptionHandler();
 	DOM\HTML::$echoOnExit = true;
 	Funcs\load('head', 'body');
-	$pdo = Core\PDO::load(\Constants\CREDS);
-		$all = $pdo(
-		'SELECT
-		`displayName` as `name`,
-		`associatedNumbers` as `tel`,
-		`allLines` as `full text`
-		FROM `contacts`;'
-	);
-	$hash = md5(json_encode($all));
-	Core\Console::getInstance()->log($hash);
 }
-
